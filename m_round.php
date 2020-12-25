afe593901ba59549406dc7d683246a740653d159
mcaptain <mcaptain@msk-school21.ru>
2020-12-25 13:49:04 +0300

    добавил проверку на ноль

--- m_round.php
+++ m_round.php
@@ -1,8 +1,11 @@
 <?php
 
-function m_round($money)
+function m_round(float $money)
 {
 	$minimal_coin = 0.5;
+	if ($money === 0) {
+		return 0;
+	}
 	$number_of_coins = (int)($money / $minimal_coin);
 	$number_of_coins = ($money - ($number_of_coins * $minimal_coin)) >= 0.25
 		? $number_of_coins + 1 
