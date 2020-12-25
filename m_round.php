1d7cf815a54ac728bfee484deecb30387474f0a0
mcaptain <mcaptain@msk-school21.ru>
2020-12-25 13:47:01 +0300

    изменил кол-во подсчета

--- m_round.php
+++ m_round.php
@@ -4,12 +4,12 @@ function m_round($money)
 {
 	$minimal_coin = 0.5;
 	$number_of_coins = (int)($money / $minimal_coin);
-	$number_of_coins = ($money - ($number_of_coins * $minimal_coin)) > 0.25
+	$number_of_coins = ($money - ($number_of_coins * $minimal_coin)) >= 0.25
 		? $number_of_coins + 1 
 		: $number_of_coins;
 	return $number_of_coins * $minimal_coin;
 }
 
 var_dump(m_round(4.72));
-var_dump(m_round(2.25));
+var_dump(m_round(2.35));
 var_dump(m_round(3.13));
