01f850291dd5ec45bf27e75002fc1fe2eb304c90
mcaptain <mcaptain@msk-school21.ru>
2020-12-25 13:58:49 +0300

    заменил собственную реализацию на php

--- m_round.php
+++ m_round.php
@@ -7,12 +7,12 @@ function m_round(float $money)
 		return 0;
 	}
 	$number_of_coins = (int)($money / $minimal_coin);
-	$number_of_coins = ($money - ($number_of_coins * $minimal_coin)) >= 0.25
-		? $number_of_coins + 1 
+	$number_of_coins = fmod($money, $minimal_coin) >= 0.25
+		? $number_of_coins + 1
 		: $number_of_coins;
 	return $number_of_coins * $minimal_coin;
 }
 
-var_dump(m_round(4.72));
-var_dump(m_round(2.35));
+var_dump(m_round(0.1));
+var_dump(m_round(2.23));
 var_dump(m_round(3.13));
